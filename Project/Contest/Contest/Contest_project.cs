5817bd7be2b72a0636ebcc99bdd3fdea5db35f26
sudheerkss <34053437+sudheerkss@users.noreply.github.com>
2019-04-20 18:32:17 +0530

    Ahammed Created Project
    This is the final Design

---
+++ Project/Contest/Contest/Contest_project.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Contest
+{
+    public class Contest_project
+    {
+    }
+}
---
+++ Project/Contest/Contest_Code/contest_objects.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contest_Code
+{
+    public class contest_objects
+    {
+    }
+}
